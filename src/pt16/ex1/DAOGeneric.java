1eff63eed55d49c694d2ad9e20eacede07d9e4fe
Theo Kandi Bikoko Pineda <25634122r@iespoblenou.org>
2025-05-15 13:57:26 +0200

    Work in progress

--- src/pt16/ex1/DAOGeneric.java
+++ src/pt16/ex1/DAOGeneric.java
@@ -1,6 +1,12 @@
 package pt16.ex1;
 
-public interface DAOGeneric {
-public int create();
-public <T> T getItems;
+import java.util.List;
+
+public interface DAOGeneric<T> {
+    T createItem(T t);
+    T readItem (int idItem);
+    List<T> readItems();
+    T updateItem(T t);
+    T deleteItem(T t);
+
 }
